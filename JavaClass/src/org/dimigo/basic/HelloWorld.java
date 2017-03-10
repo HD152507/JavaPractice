cb7348b22bb3584754895311ecae581aa0d759be
JunhaKim <JunhaKim@218.38.137.28>
2017-03-10 14:22:15 +0900

    inital commit

---
+++ JavaClass/src/org/dimigo/basic/HelloWorld.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ *  org.dimigo.basic
+ *  |_HelloWorld
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 3. 10.
+ * </pre>
+ *
+ * @author JunhaKim
+ * @version : 1.0
+ */
+public class HelloWorld 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a = 3;
+		String name = "아이유";
+		System.out.println(a);
+		System.out.println(name);
+		System.out.println(name);
+		System.out.println(name);
+		System.out.println(name);
+	}
+
+}
