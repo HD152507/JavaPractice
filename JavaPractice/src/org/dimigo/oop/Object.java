96313f7985f4a2d063a0e94e910a03767d98fb5b
JunhaKim <JunhaKim@218.38.137.28>
2017-03-29 14:55:20 +0900

    llklk

--- JavaPractice/src/org/dimigo/oop/Object.java
+++ JavaPractice/src/org/dimigo/oop/Object.java
@@ -5,16 +5,16 @@ package org.dimigo.oop;
 
 /**
  * <pre>
- *  org.dimigo.basic
- *  |_CarTest
+ *  org.dimigo.oop
+ *  |_Object
  * 1. 개요 :
- * 2. 작성일 : 2017. 3. 23.
+ * 2. 작성일 : 2017. 3. 28.
  * </pre>
  *
  * @author JunhaKim
  * @version : 1.0
  */
-public class CarTest2 {
+public class Object {
 
 	/**
 	 * @param args
@@ -23,12 +23,6 @@ public class CarTest2 {
 	{
 		// TODO Auto-generated method stub
 
-		
-	}
-	
-	private void compare()
-	{
-		
 	}
 
 }
