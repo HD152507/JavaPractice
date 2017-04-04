7b302012540db701245ed269a02dd0edecef7fd8
JunhaKim <JunhaKim@218.38.137.28>
2017-04-04 12:05:41 +0900

    iddddddddd

---
+++ JavaPractice/src/org/dimigo/oop/IdolGroup.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ *  org.dimigo.oop
+ *  |_IdolGroup
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 4. 4.
+ * </pre>
+ *
+ * @author JunhaKim
+ * @version : 1.0
+ */
+public class IdolGroup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
