fd9d8938112ab91919d6a795bd5514ea17770451
JunhaKim <JunhaKim@172.17.1.182>
2017-05-11 09:18:07 +0900

    commmmmmmmmmmmit

--- JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -14,23 +14,33 @@ package org.dimigo.inheritance;
  * @author JunhaKim
  * @version : 1.0
  */
-public class PersonTest {
+public class PersonTest2 {
 
 	/**
 	 * @param args
 	 */
+	
+	private static void greeting(Person p)
+	{
+		System.out.println(p.toString());
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
 		
-		Person[] per = {
+		Person[] p = {
+				new Person("Tom"),
 				new Korean("홍길동"),
 				new Japanese("다나카"),
 				new Chinese("왕밍"),
-				new Person("Tom"),
+				
 		};
 		
 		for(int i=0;i<4;i++)
 		{
-			
+			greeting(p[i]);
 		}
 		
 	}
