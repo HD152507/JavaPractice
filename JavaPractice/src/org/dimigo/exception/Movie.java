fe7ae003c033653ab9fc68d1f741ddf51b92a54d
JunhaKim <JunhaKim@218.38.137.28>
2017-05-30 12:50:19 +0900

    ss

--- JavaPractice/src/org/dimigo/exception/Movie.java
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -18,7 +18,7 @@ public class Movie {
 	private String title;
 	private int limitAge;
 
-	public Movie(String title, int limitAge) {
+	Movie(String title, int limitAge) {
 		this.title = title;
 		this.limitAge = limitAge;
 	}
@@ -31,15 +31,9 @@ public class Movie {
 		return limitAge;
 	}
 
-	public void buyTicket(int age) {
-		if (age < limitAge) {
-			try {
-				throw new Exception(title + "은/는 " + limitAge + "세 이상 관람가입니다.");
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-		} else {
-			System.out.println(getTitle() + " 잘 보세요");
-		}
+	public void buyTicket(int age) throws Exception {
+		if (age < limitAge)
+			throw new Exception(title + "은/는 " + limitAge + "세 이상 관람가입니다.");
+		System.out.println(title + " 즐감하세요.");
 	}
 }
--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -15,14 +15,22 @@ package org.dimigo.exception;
  * @version : 1.0
  */
 public class MovieTest {
-	public static void main(String[] args) {
 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		Movie[] movies = { new Movie("에이리언:커버넌트", 15), new Movie("가디언즈 오브 갤럭시", 12) };
 
-		int age = 13;
-
+		int age = 19;
 		for (Movie movie : movies) {
-			movie.buyTicket(age);
+			try {
+				movie.buyTicket(age);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
 		}
+
 	}
+
 }
